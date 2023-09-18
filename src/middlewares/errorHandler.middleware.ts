c824d86a0dff48f05bd8962783ad42f36bb58393
Daniel Dias <daniiel2dias@gmail.com>
2023-09-18 10:11:26 -0300

    tentei escapar do any e não consegui

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import { PostgresError } from 'pg-error-enum';
 
-export function errorHandler(error, req: Request, res: Response, next: NextFunction) {
+export function errorHandler(error: Error | any, req: Request, res: Response, next: NextFunction) {
     console.log(error)
     if(error.message === "NotFound") return res.sendStatus(httpStatus.NOT_FOUND);
     if(error.code === PostgresError.UNIQUE_VIOLATION) return res.status(httpStatus.CONFLICT).send(`${error.detail}`);
--- src/repositories/postRepos.repostirories.ts
+++ src/repositories/postRepos.repostirories.ts
@@ -1,5 +1,6 @@
 import db from "@/database/database.connection";
+import { QueryResult } from "pg";
 
 export default async function postRepo (name: string, phone: string, email: string): Promise<void>{
-    const result = await db.query(`INSERT INTO registers (name, phone, email) VALUES ($1, $2, $3)`, [name, phone, email]);
+    const result: QueryResult = await db.query(`INSERT INTO registers (name, phone, email) VALUES ($1, $2, $3)`, [name, phone, email]);
 };
